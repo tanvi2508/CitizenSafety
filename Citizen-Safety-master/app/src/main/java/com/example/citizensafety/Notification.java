38ad991832bcd00cc7a4ca66f902ecd95edf1f4c
tanvi2508 <tanviverma2508@gmail.com>
2022-10-05 21:03:00 +0530

    First Commit

---
+++ Citizen-Safety-master/app/src/main/java/com/example/citizensafety/Notification.java
@@ -0,0 +1,84 @@
+package com.example.citizensafety;
+
+import android.annotation.TargetApi;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.IBinder;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+public class Notification extends Service {
+    public Notification() {
+    }
+    protected BroadcastReceiver stopReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Unregister the BroadcastReceiver when the notification is tapped//
+            unregisterReceiver(stopReceiver);
+            //Stop the Service//
+            stopSelf();
+
+        }
+    };
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        buildNotification();
+        createChannel();
+        //startActivity(new Intent(this, Message.class));
+
+    }
+    private void buildNotification() {
+        String stop = "stop";
+        registerReceiver(stopReceiver, new IntentFilter(stop));
+        PendingIntent broadcastIntent = PendingIntent.getBroadcast(
+                this, 0, new Intent(stop), PendingIntent.FLAG_UPDATE_CURRENT);
+        // Create the persistent notification
+        android.app.Notification.Builder builder = new android.app.Notification.Builder(this)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.unsafe_msg))
+//Make this notification ongoing so it can’t be dismissed by the user//
+                .setOngoing(true)
+                .setContentIntent(broadcastIntent)
+                .setSmallIcon(R.drawable.tap_help);
+        startForeground(2, builder.build());
+
+
+    }
+
+    @NonNull
+    @TargetApi(26)
+    private synchronized String createChannel() {
+        NotificationManager mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        String name = "snap map fake location ";
+        int importance = NotificationManager.IMPORTANCE_LOW;
+
+        NotificationChannel mChannel = new NotificationChannel("snap map channel", name, importance);
+
+        mChannel.enableLights(true);
+        mChannel.setLightColor(Color.BLUE);
+        if (mNotificationManager != null) {
+            mNotificationManager.createNotificationChannel(mChannel);
+        } else {
+            stopSelf();
+        }
+        return "snap map channel";
+    }
+
+}
